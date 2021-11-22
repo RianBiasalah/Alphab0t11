599be14057ebd7a28eb11a0c0e26c968dc96c2a0
RianBiasalah <94624733+RianBiasalah@users.noreply.github.com>
2021-11-22 18:30:03 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -1,27 +1,15 @@
 const donasibot = () => {
-return `DONASI BISA VIA BANK / E-MONEY
+return `DONASI E-MONEY
 
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
 	
 *💰 E-MONEY*
 
-1. GOPAY
+
+1. PULSA
 	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
-3. DANA
+2. DANA
 	ㅁ 0887 4350 47326
 	
-Semuanya atas nama RI****DI
 `
 	}
 
